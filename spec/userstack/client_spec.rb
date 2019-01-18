7d872a0df766e7d766862bc9a81bc0b072981201
m-maeda-feedforce <m-maeda@feedforce.jp>
2019-01-18 19:04:02 +0900

    :recycle: Refactoring test
    modified:   spec/userstack/client_spec.rb

--- spec/userstack/client_spec.rb
+++ spec/userstack/client_spec.rb
@@ -20,9 +20,7 @@ describe Userstack::Client do
       let(:request_uri) { "#{stub_request_uri_scheme}://api.userstack.com/detect?access_key=#{access_key}&ua=#{CGI.escape(useragent)}" }
 
       before do
-        stub_request(:get, request_uri).to_return(
-          status: stub_response_status, body: stub_response_body
-        )
+        stub_request(:get, request_uri).to_return(status: stub_response_status, body: stub_response_body)
       end
 
       subject { client.parse(useragent) }
@@ -31,25 +29,64 @@ describe Userstack::Client do
 
     let(:access_key) { 'c22b52f2b1409beb22585ae658280' }
     let(:useragent) { 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36' }
+    let(:normal_response) do
+      {
+        'ua' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+        'type' => 'browser',
+        'brand' => 'Apple',
+        'name' => 'Mac',
+        'url' => 'https://www.google.com/about/company/',
+        'os' => {
+          'name' => 'macOS 10.14 Mojave',
+          'code' => 'macos_10_14',
+          'url' => 'https://en.wikipedia.org/wiki/MacOS_Mojave',
+          'family' => 'macOS',
+          'family_code' => 'macos',
+          'family_vendor' => 'Apple Inc.',
+          'icon' => 'https://assets.userstack.com/icon/os/macosx.png',
+          'icon_large' => 'https://assets.userstack.com/icon/os/macosx_big.png'
+        },
+        'device' => {
+          'is_mobile_device' => false,
+          'type' => 'desktop',
+          'brand' => 'Apple',
+          'brand_code' => 'apple',
+          'brand_url' => 'http://www.apple.com/',
+          'name' => 'Mac'
+        },
+        'browser' => {
+          'name' => 'Chrome',
+          'version' => '71.0.3578.98',
+          'version_major' => '71',
+          'engine' => 'WebKit/Blink'
+        },
+        'crawler' => {
+          'is_crawler' => false,
+          'category' => nil,
+          'last_seen' => nil
+        }
+      }
+    end
+    let(:legacy_response) do
+      {
+        'data' => {
+          'ua_type' => 'Desktop',
+          'os_name' => 'macOS',
+          'os_version' => '10.12.3',
+          'browser_name' => 'Chrome',
+          'browser_version' => '56.0.2924.87',
+          'engine_name' => 'WebKit',
+          'engine_version' => '537.36',
+        }
+      }
+    end
 
     context 'given registered Access key' do
       let(:client) { Userstack::Client.new(access_key) }
       let(:stub_request_uri_scheme) { 'https' }
       let(:stub_response_status) { 200 }
-      let(:stub_response_body) { expectation.to_json }
-      let(:expectation) do
-        {
-          'data' => {
-            'ua_type' => 'Desktop',
-            'os_name' => 'macOS',
-            'os_version' => '10.12.3',
-            'browser_name' => 'Chrome',
-            'browser_version' => '56.0.2924.87',
-            'engine_name' => 'WebKit',
-            'engine_version' => '537.36',
-          }
-        }
-      end
+      let(:stub_response_body) { normal_response.to_json }
+      let(:expectation) { normal_response }
 
       it_behaves_like 'Userstack'
     end
@@ -104,20 +141,8 @@ describe Userstack::Client do
       let(:client) { Userstack::Client.new(access_key, use_ssl: false) }
       let(:stub_request_uri_scheme) { 'http' }
       let(:stub_response_status) { 200 }
-      let(:stub_response_body) { expectation.to_json }
-      let(:expectation) do
-        {
-          'data' => {
-            'ua_type' => 'Desktop',
-            'os_name' => 'macOS',
-            'os_version' => '10.12.3',
-            'browser_name' => 'Chrome',
-            'browser_version' => '56.0.2924.87',
-            'engine_name' => 'WebKit',
-            'engine_version' => '537.36',
-          }
-        }
-      end
+      let(:stub_response_body) { normal_response.to_json }
+      let(:expectation) { normal_response }
 
       it_behaves_like 'Userstack'
     end
