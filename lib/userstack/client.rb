16ee5a00f70e10c76539edc6b1f719839c8d7dd2
m-maeda-feedforce <m-maeda@feedforce.jp>
2019-01-18 16:45:18 +0900

    :pencil: Fix yard
    modified:   lib/userstack/client.rb

--- lib/userstack/client.rb
+++ lib/userstack/client.rb
@@ -8,6 +8,8 @@ module Userstack
   # A class which wraps calls to Userstack API
   class Client
     # @param access_key [String] Userstack Access key
+    # @param use_ssl [TrueClass, FalseClass] Use ssl or not
+    # @param legacy [TrueClass, FalseClass] Legacy response
     # @raise [ArgumentError] when `access_key` is invalid
     # @see https://userstack.com/documentation
     def initialize(access_key, use_ssl: true, legacy: false)
@@ -19,7 +21,6 @@ module Userstack
       freeze
     end
 
-    # @return [String] Returns the Access key
     attr_reader :access_key, :use_ssl, :legacy
 
     # Parse an useragent using Userstack
