684fd28cbf0348ae8e23d34be8dd647da9f30100
OZAWA Sakuro <ozawa@feedforce.jp>
2019-01-28 10:54:34 +0900

    Replace kwargs with optional hash

--- lib/userstack/client.rb
+++ lib/userstack/client.rb
@@ -7,19 +7,19 @@ module Userstack
   # A class which wraps calls to Userstack API
   class Client
     USER_AGENT = 'Userstack gem/%s' % VERSION
-    private_constant :USER_AGENT
 
     # @param access_key [String] Userstack Access key
     # @param use_ssl [Boolean] Use ssl or not
     # @param legacy [Boolean] Legacy response
     # @raise [ArgumentError] when `access_key` is invalid
     # @see https://userstack.com/documentation
-    def initialize(access_key, use_ssl: true, legacy: false)
+    def initialize(access_key, options={})
+      defaults = { :use_ssl => true, :legacy => false}
       raise ArgumentError, 'Invalid Access key' if access_key.nil? || access_key.empty?
 
       @access_key = access_key.freeze
-      @use_ssl = use_ssl
-      @legacy = legacy
+      @use_ssl = options.fetch(:use_ssl, defaults[:use_ssl])
+      @legacy = options.fetch(:legacy, defaults[:legacy])
       freeze
     end
 
@@ -42,9 +42,9 @@ module Userstack
 
     def request(useragent)
       uri = UriBuilder.execute(access_key, useragent, use_ssl, legacy)
-      Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-        http.get(uri.to_s, 'User-Agent' => USER_AGENT)
-      end
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == 'https'
+      http.get(uri.to_s, 'User-Agent' => USER_AGENT)
     end
 
     def parse_as_json(json_text)
